c31207f63d3957e593ba53ea7a09699e83a4dcf1
M0 <60576085+m0hss@users.noreply.github.com>
2025-11-07 01:40:46 +0000

    update fsb1.0

--- src/Components/MySkills.jsx
+++ src/Components/MySkills.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const MySkills = () => {
   const skillsData = [
-    { id: 1, title: 'React', percentage: 99 },
-    { id: 2, title: 'JavaScript/NODE', percentage: 99 },
-    { id: 3, title: 'PYTHON', percentage: 95 },
-    { id: 4, title: 'C++', percentage: 100 },
-    { id: 5, title: 'NextJS', percentage: 80 },
-    { id: 6, title: 'Typescript', percentage: 80 },
-    { id: 7, title: 'MongoDB', percentage: 95 },
-    { id: 8, title: 'SQL', percentage: 99 },
+    { id: 1, title: "Autodesk", percentage: 99 },
+    { id: 2, title: "JavaScript/NODE", percentage: 99 },
+    { id: 3, title: "PYTHON", percentage: 95 },
+    { id: 4, title: "C++", percentage: 100 },
+    { id: 5, title: "NextJS", percentage: 80 },
+    { id: 6, title: "Typescript", percentage: 80 },
+    { id: 7, title: "MongoDB", percentage: 95 },
+    { id: 8, title: "SQL", percentage: 99 },
   ];
 
   return (
-    <div className='text-white bg-gray-900'>
+    <div className="text-white bg-gray-900">
       <h2 className="text-3xl text-center font-bold mb-4 my-20">MY SKILLS</h2>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
@@ -26,9 +26,9 @@ const MySkills = () => {
                 value={skill.percentage}
                 text={`${skill.percentage}%`}
                 styles={buildStyles({
-                  textColor: '#e0e0e0',
-                  pathColor: '#FFAE42',
-                  trailColor: '#e0e0e0',
+                  textColor: "#e0e0e0",
+                  pathColor: "#FFAE42",
+                  trailColor: "#e0e0e0",
                 })}
               />
             </div>
@@ -39,6 +39,5 @@ const MySkills = () => {
     </div>
   );
 };
-
 
 export default MySkills;
